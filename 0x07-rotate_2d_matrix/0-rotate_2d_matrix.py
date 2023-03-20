4b8b30d6fd80766aeb4ca8dd87a261b6ddefba44
vraimademoiselle <mademoisellebone@gmail.com>
2023-03-20 11:18:30 +0300

    corrections

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Rotate 2D Matrix"""
+'''2D matrix'''
 
 
 def rotate_2d_matrix(matrix):
-    """This solution uses nested loops to access each
-      element of the matrix and swap its values with the elements
-        located at the corresponding positions in
-        the rotated matrix."""
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing'''
+    left, right = 0, len(matrix) - 1
 
-    n = len(matrix)
-    for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[n - j - 1][i]
-            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
-            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
-            matrix[j][n - i - 1] = temp
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
